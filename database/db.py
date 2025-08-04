7d30d6d6de5fd59db257eae0cb87f6a8277e1d34
Monazir Muhammad Doha <mmdoha@houndsec.net>
2025-08-04 13:41:43 +0600

    add multicolumn index & new tables for multipart uploads

--- database/db.py
+++ database/db.py
@@ -4,12 +4,13 @@ from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy import Index
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 import uuid
-import uuid6
+from uuid_extensions import uuid7
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from config import DATABASE_URL
 
@@ -36,7 +37,7 @@ class File(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         PG_UUID(as_uuid=True), 
         primary_key=True, 
-        default=uuid6.uuid7,
+        default= lambda: uuid7(),
         unique=True
     )
     visibility: Mapped[str] = mapped_column(default="private")
@@ -44,13 +45,62 @@ class File(Base):
         ForeignKey("vaults.id", ondelete="CASCADE"), 
         nullable=False
     )
-    file: Mapped[str] 
+    file: Mapped[str] = mapped_column(String(255))
     size: Mapped[int] = mapped_column(BigInteger)# Size in bytes
     date_created: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
 
+    __table_args__ = (
+        Index("ix_files_vault_id_id", "vault_id", "id"), # multicolumn index on vault_id & file_id 
+    )
 
     def __repr__(self) -> str:
-        return f"file(id={self.id!r}, visibility={self.visibility!r},vault={self.vault_id!r},  file={self.file!r}, size={self.size!r}, date_created={self.date_created!r})"
+        return f"file(id={self.id!r}, visibility={self.visibility!r},vault_id={self.vault_id!r},  file={self.file!r}, size={self.size!r}, date_created={self.date_created!r})"
+
+class Upload(Base):
+    __tablename__ = "uploads"
+    file_id: Mapped[uuid.UUID] = mapped_column(
+        PG_UUID(as_uuid=True), 
+        primary_key=True, 
+        default= lambda: uuid7(),
+        unique=True
+    )
+    vault_id: Mapped[int] = mapped_column(
+        ForeignKey("vaults.id", ondelete="CASCADE"), 
+        nullable=False
+    )
+    object_upload_id: Mapped[str] # upload id from s3
+    file: Mapped[str] = mapped_column(String(255))
+    size: Mapped[int] = mapped_column(BigInteger)# Size in bytes
+    date_created: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+
+    __table_args__ = (
+        Index("ix_uploads_vault_id_file_id", "vault_id", "file_id"), # multicolumn index on vault_id & file_id 
+    )
+
+    def __repr__(self) -> str:
+        return f"Upload(file_id={self.file_id!r},vault_id={self.vault_id!r}, upload_id={self.object_upload_id!r},  file={self.file!r}, size={self.size!r}, date_created={self.date_created!r})"
+
+
+class Chunk(Base):
+    __tablename__ = "chunks"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    vault_id: Mapped[int] = mapped_column(
+        ForeignKey("vaults.id", ondelete="CASCADE"), 
+        nullable=False
+    )
+    file_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("uploads.file_id", ondelete="CASCADE"), 
+        nullable=False
+    )
+    part_number: Mapped[int]
+    etag: Mapped[str] = mapped_column(String(64))
+
+    __table_args__ = (
+        Index("ix_chunks_vault_id_file_id", "vault_id", "file_id"), # multicolumn index on vault_id & file_id 
+    )
+
+    def __repr__(self) -> str:
+        return f"Chunk(id={self.id!r}, vault_id={self.vault_id!r}, part_number={self.part_number!r}, etag={self.etag!r})"
 
 
 engine = create_engine(
